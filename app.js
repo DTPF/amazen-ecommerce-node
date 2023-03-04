14b17206fff3f910ee2832af598bf5fc7b489ec0
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-04 18:05:23 +0100

    change(app.js): add cart routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { API_VERSION } = require("./config");
 
 const authRoutes = require("./routers/auth");
 const userRoutes = require("./routers/user");
+const cartRoutes = require("./routers/cart");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -25,5 +26,6 @@ app.use((req, res, next) => {
 // Router Basic
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, cartRoutes);
 
 module.exports = app;
