363e2f204ba9a36bb7dbec390c35d5efa581c923
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-05 22:39:12 +0100

    change(cart): add new routers

--- routers/cart.js
+++ routers/cart.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const CartController = require("../controllers/cart");
-
 const api = express.Router();
+const md_auth = require("../middlewares/authenticated");
 
-api.post("/add-to-cart", CartController.addToCart);
-api.get("/get-cart-items-by-user-id/:id", CartController.getCartItemsByUserId);
+api.post("/add-to-cart", [md_auth.ensureAuth], CartController.addToCart);
+api.get("/get-cart-items-by-user-id/:id", [md_auth.ensureAuth], CartController.getCartItemsByUserId);
+api.delete("/delete-cart-item-by-id/:id", [md_auth.ensureAuth], CartController.deleteCartItemById);
 
 module.exports = api;
