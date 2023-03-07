c49385ec844f93be4b56f1aefc3115160762fb24
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-07 12:43:33 +0100

    feat(cart): add messages

---
+++ responsesMessages/cart.messages.js
@@ -0,0 +1,11 @@
+const cartMessage = {
+  productNotFound: 'No se ha encontrado el producto',
+  serverError: 'Error del servidor',
+  addProductSuccess: 'Añadido correctamente',
+  deleteProductSuccess: 'Eliminado correctamente',
+  modifyProductSuccess: 'Modificado correctamente',
+}
+
+module.exports = {
+  cartMessage
+}
