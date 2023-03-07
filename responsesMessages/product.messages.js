28dc40b5c41e601dcced845455032a4a3c90792a
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-07 12:43:45 +0100

    feat(product): add messages

---
+++ responsesMessages/product.messages.js
@@ -0,0 +1,11 @@
+const productMessage = {
+  addProductSuccess: 'Añadido correctamente',
+  productNotFound: 'No se ha encontrado el producto',
+  serverError: 'Error del servidor',
+  extensionNotValid: 'La extensión no es válida',
+  imageNotExists: 'La imagen no existe'
+}
+
+module.exports = {
+  productMessage
+}
