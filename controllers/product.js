e45b69ff2004539cebb3967ca36303b0cb622715
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-12 17:12:13 +0100

    fix(productionErrors)

--- controllers/product.js
+++ controllers/product.js
@@ -5,16 +5,19 @@ const Product = require("../models/Product");
 const { productMessage } = require("../responsesMessages/product.messages");
 
 function addProduct(req, res) {
-  const { title, info, sizeAndPrice } = req.body;
+  const { title, info, sizeAndPrice, stock, stars, waitShippingTime, category, discount } = req.body;
   const product = new Product();
   product.title = title;
   product.info = info;
   product.sizeAndPrice = sizeAndPrice;
-  product.stock = 100;
-  product.stars = 0;
-  product.shippingDate = moment().add(1, 'days');;
-  product.createdAt = Date.now();
-  product.updatedAt = Date.now();
+  product.stock = stock;
+  product.stars = stars;
+  product.waitShippingTime = waitShippingTime;
+  product.category = category;
+  product.defaultImage = 1;
+  product.discount = discount;
+  product.createdAt = new Date();
+  product.updatedAt = new Date();
 
   product.save()
     .then(productStored => {
@@ -30,20 +33,60 @@ function addProduct(req, res) {
     });
 }
 
-function getProducts(req, res) {
-  Product.find()
-    .then((products) => {
-      if (!products) {
+function updateProduct(req, res) {
+  const { id } = req.params;
+  const productData = req.body;
+  productData.updatedAt = new Date();
+
+  Product.findByIdAndUpdate({ _id: id }, productData, { returnOriginal: false })
+    .then(productUpdate => {
+      if (!productUpdate) {
         return res.status(404).send({ status: 404, message: productMessage.productNotFound });
       } else {
-        return res.status(200).send({ status: 200, products: products });
+        return res.status(200).send({ status: 200, message: 'Modificado correctamente', product: productUpdate });
       }
     })
     .catch(err => {
       if (err) {
         return res.status(500).send({ status: 500, message: productMessage.serverError });
       }
-    });
+    })
+}
+
+async function getProducts(req, res) {
+  const { category } = req.params;
+
+  if (category !== 'all') {
+    Product.find({ category: category })
+      .sort({ updatedAt: "desc" })
+      .then((products) => {
+        if (!products) {
+          return res.status(404).send({ status: 404, message: productMessage.productNotFound });
+        } else {
+          return res.status(200).send({ status: 200, products: products });
+        }
+      })
+      .catch(err => {
+        if (err) {
+          return res.status(500).send({ status: 500, message: productMessage.serverError });
+        }
+      });
+  } else {
+    Product.find()
+      .sort({ updatedAt: "desc" })
+      .then((products) => {
+        if (!products) {
+          return res.status(404).send({ status: 404, message: productMessage.productNotFound });
+        } else {
+          return res.status(200).send({ status: 200, products: products });
+        }
+      })
+      .catch(err => {
+        if (err) {
+          return res.status(500).send({ status: 500, message: productMessage.serverError });
+        }
+      });
+  }
 }
 
 function getProductById(req, res) {
@@ -63,12 +106,12 @@ function getProductById(req, res) {
     });
 }
 
-function addImage(req, res) {
+async function addImage(req, res) {
   const { productId } = req.params;
   const path = req.files.productImage && req.files.productImage.path;
 
-  Product.findById({ _id: productId })
-    .then(product => {
+  Product.findById({ _id: productId }, { returnOriginal: false })
+    .then(async product => {
       if (!product) {
         path && fs.unlinkSync(path);
         return res.status(404).send({ status: 404, message: productMessage.productNotFound });
@@ -86,12 +129,12 @@ function addImage(req, res) {
           return res.status(400).send({ status: 400, message: productMessage.extensionNotValid });
         }
 
-        fileName && Product.findByIdAndUpdate({ _id: productId }, { $push: { images: fileName } })
+        fileName && await Product.findByIdAndUpdate({ _id: productId }, { $push: { images: fileName } }, { returnOriginal: false })
           .then(product => {
             if (!product) {
               return res.status(404).send({ status: 404, message: productMessage.productNotFound });
             }
-            return res.status(200).send({ status: 200, productId: product._id, imagePath: fileName });
+            return res.status(200).send({ status: 200, message: 'Imágen añadida', product: product });
           })
           .catch(err => {
             if (err) {
@@ -111,17 +154,17 @@ function deleteImage(req, res) {
   const { productId } = req.params;
   let { imageName } = req.body;
 
-  Product.findByIdAndUpdate({ _id: productId }, { $pullAll: { images: [imageName] } })
+  Product.findByIdAndUpdate({ _id: productId }, { $pullAll: { images: [imageName] } }, { returnOriginal: false })
     .then(product => {
       if (!product) {
         return res.status(404).send({ status: 404, message: productMessage.productNotFound });
       }
       imageName && fs.unlinkSync(`uploads/products/${imageName}`)
-      return res.status(200).send({ status: 200, product: product._id });
+      return res.status(200).send({ status: 200, message: 'Imágen eliminada', product: product });
     })
     .catch(err => {
       if (err) {
-        return res.status(500).send({ status: 500, message: productMessage.serverError });
+        return res.status(500).send({ status: 500, message: err });
       }
     })
 }
@@ -163,6 +206,7 @@ async function getProductImage(req, res) {
 
 module.exports = {
   addProduct,
+  updateProduct,
   getProducts,
   getProductById,
   addImage,
