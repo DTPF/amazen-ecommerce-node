efa58c2d9a6b1b2d880792ca392df354b8ec053b
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-12 16:56:37 +0100

    feat(order): add model

---
+++ models/Order.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrderSchema = Schema({
+  userId: String,
+  products: {},
+  creditCardNumber: {
+    creditNumber: Number,
+    creditCCV: Number,
+    creditExpires: String
+  },
+  totalAmount: Number,  
+  status: String,
+  shippingDate: Date,
+  createdAt: Date,
+  updatedAt: Date
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
